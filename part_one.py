505f5e8b785741d4e646e569fd820c8ad9a0fdf3
= <=>
2024-02-03 12:50:20 +0300

    final updates

--- part_one.py
+++ part_one.py
@@ -12,6 +12,8 @@
 # Display full details of the student
 
 #Solutions
+#NABATANZI GORRET SEP23/BCS/3472U/F
+
 
 #Student Details
 #Creating variables to store the details from the keyboard and casting the marks from string to integer
@@ -20,6 +22,7 @@ studentNumber = input("Enter the student's number:\t")
 programmingMarks = int(input("Enter the student's marks in Programming:\t"))
 dataScienceMarks = int(input("Enter the student's marks in Data Science:\t"))
 computerApplicationMarks = int(input("Enter the student's marks in Computer Applications:\t"))
+print("\n")
 
 
 #Calculating the student's average mark scored
@@ -27,13 +30,14 @@ averageMark = (programmingMarks + dataScienceMarks + computerApplicationMarks ) 
 #printing the average mark rounded to 3 decimal places
 print(studentName + " has an Average Mark of %.3f " %averageMark + "\n"  )
 
+
 #Displaying the details
 print(f".....Student Details.....")
 print(f"Student Name: {studentName}")
 print(f"Student Number: {studentNumber}/n")
-print("\nCourse Unit Marks....")
+print("\n.....Student Course Unit Marks....")
 print(f"Programming: {programmingMarks}")
 print(f"Data Science: {dataScienceMarks}")
-print(f"Computer Applications: {computerApplicationMarks}")
+print(f"Computer Applications: {computerApplicationMarks} \n")
 
-
+#NABATANZI GORRET SEP23/BCS/3472U/F
--- part_two.py
+++ part_two.py
@@ -1,17 +1,16 @@
 #NABATANZI GORRET SEP23/BCS/3472U/F
 
 #Question 1
-#Write a program that converts celicius to faranheit.The program should ask the user to enter the
-# temperature in celicus and then display the temperature converted to Farenheit
+#Write a program that converts celsius temperature to Fahrenheit.The program should ask the user to enter the
+# temperature in celsius and then display the temperature converted to Fahrenheit
 
-#Conversion Expression
 
 #store celicius value from the user
 print("\n*** Celicius to Farenheit Degrees Conversion ***\n")
 celicius = input('Enter the Temperature in Celicius Degerees: ')
-F = (9/5) * int(celicius) + 32
-faranheit = float(F) #casting the expression to a float type
-print(celicius + " Degrees Celicius is equal to " + str(faranheit) + " Degrees Farenheit\n")
+F = (9/5) * int(celicius) + 32 #Conversion Expression
+fahreneit = int(F) #casting the expression to a float type
+print(celicius + " Degrees Celicius is equal to " + str(fahreneit) + " Degrees Farenheit\n")
 
 
 #NABATANZI GORRET SEP23/BCS/3472U/F
@@ -22,10 +21,11 @@ print(celicius + " Degrees Celicius is equal to " + str(faranheit) + " Degrees F
 #MPG = milesDriven / gallonsOfGasUsed
 
 print("\n*** Car's Miles Per Gallon Calculations ***\n")
-#storing user input and casting them to type int
-milesDriven = int(input('Enter the number of miles driven: '))
-gallonsOfGasUsed = int(input('Enter the number of gallons of gas used: '))
-MPG =  float(milesDriven / gallonsOfGasUsed)
+
+#storing user input and casting it to type float
+milesDriven = float(input('Enter the number of miles driven: '))
+gallonsOfGasUsed = float(input('Enter the number of gallons of gas used: '))
+MPG =  milesDriven / gallonsOfGasUsed
 milesPerGallons = MPG
 #displaying the car's miles per gallons used
 print("The car has used %.2f" %milesPerGallons + " miles per gallons")
@@ -38,6 +38,7 @@ print("The car has used %.2f" %milesPerGallons + " miles per gallons")
 #Make sure the program enters the radius from the keyboard and provide the answer in 2 decimal places
 
 print("\n*** Calculating the Volume of a Sphere ***\n")
+
 #storing the radius from the keyboard and casting it to int
 radius = int(input('Enter the radius of the sphere: '))
 volumeOfSphere = float((4/3) * 3.14 * radius ** 2)
